aa9fa4b66e242c9aa3ef03646203c1dca6137ed2
Abhishek <abhishek123@gmail.com>
2024-09-17 17:55:40 +0530

    commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// menubar
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+
+}
+// index slider
+let slideImages = document.querySelectorAll('.all-images');   /*access images */
+let next = document.querySelector('.next');        /* access next button */
+let prev = document.querySelector('.prev');        /* access prev button */
+let dots = document.querySelectorAll('.dot');        /* access prev button */
+
+var counter = 0;
+next.addEventListener('click', slideNext);
+function slideNext() {
+    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+    if (counter >= slideImages.length-1) {
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    indicators();
+}
+prev.addEventListener('click', slidePrev);
+function slidePrev() {
+    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+    if (counter == 0) {
+        counter = slideImages.length - 1;
+    }
+    else {
+        counter--;
+    }
+    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    indicators();
+}
+// Auto sliding
+function autoSliding() {
+    deletInterval = setInterval(timer, 3000);
+    function timer() {
+        slideNext();
+        indicators();
+    }
+}
+autoSliding();
+
+// stop auto sliding mouse is over
+let contain = document.querySelector('.hero');
+contain.addEventListener('mouseover', function () {
+    clearInterval(deletInterval);
+});
+// Resume slider when mouse is out
+contain.addEventListener('mouseout', autoSliding)
+
+// add and remove active class from dots
+function indicators() {
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[counter].className += ' active';
+}
+// click event tothe dots
+function switchImage(currentImage) {
+    currentImage.classList.add('active');
+    var imageId = currentImage.getAttribute('attr');
+    if (imageId > counter) {
+        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    else if (imageId == counter) {
+        return;
+    }
+    else {
+        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    }
+    indicators();
+}
